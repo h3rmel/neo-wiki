b155d02cefbf2c4c7587a7e92447976277006677
H3rmel <isaachermel@gmail.com>
2025-01-15 19:11:48 -0300

    fix ignore

---
+++ src/app/_sessions/hero.tsx
@@ -0,0 +1,6 @@
+/**
+ * Hero Section of the home page
+ */
+export function Hero() {
+  return <section id="hero">Hero</section>;
+}
---
+++ src/app/_sessions/trends.tsx
@@ -0,0 +1,6 @@
+/**
+ * Trends Section of the home page
+ */
+export function Trends() {
+  return <section id="trends">Trends</section>;
+}
